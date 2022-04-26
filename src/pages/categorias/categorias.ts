a1dc0075da5c169ffe2fff4053ac62dfec67f56c
Edevaldo Junior <ed.tadsifpr@gmail.com>
2022-04-26 17:19:01 -0300

    Criando página de produtos

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -4,13 +4,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoriaDTO } from '../../models/Categoria.dto';
 
-/**
- * Generated class for the CategoriasPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-categorias',
@@ -35,6 +28,10 @@ export class CategoriasPage {
     },
     error => {})
     
+  }
+
+  showProdutos() {
+    this.navCtrl.push('ProdutosPage');
   }
 
 }
