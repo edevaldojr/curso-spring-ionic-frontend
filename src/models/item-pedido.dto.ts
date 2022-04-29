ea5c2585b78907e58f9a7e578ef8c7ce54bce584
Edevaldo Junior <ed.tadsifpr@gmail.com>
2022-04-29 16:50:29 -0300

    Armazenamento dos dados do pedido

---
+++ src/models/item-pedido.dto.ts
@@ -0,0 +1,6 @@
+import { RefDTO } from "./re.dto";
+
+export interface ItemPedidoDTO {
+  quantidade : number;
+  produto: RefDTO;
+}
---
+++ src/models/pagamento.dto.ts
@@ -0,0 +1,4 @@
+export interface PagamentoDTO {
+  numeroDeParcelar : number;
+  "@type" : string;
+}
